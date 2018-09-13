261ab77b596d0150fe1c6ec429c86d57557aa1a2
Qian Sun <s223554@gmail.com>
2018-09-12 23:27:45 -0400

    Basic function is done, except for fill the black corners. Please debug tomorrow.

--- Project-Code-Python/Agent.py
+++ Project-Code-Python/Agent.py
@@ -9,8 +9,8 @@
 # These methods will be necessary for the project's main method to run.
 
 # Install Pillow and uncomment this line to access image processing.
-#from PIL import Image
-#import numpy
+# from PIL import Image
+# import numpy
 
 class Agent:
     # The default constructor for your Agent. Make sure to execute any
@@ -31,9 +31,56 @@ class Agent:
     #
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
-    def Solve(self,problem):
+    def Solve(self, problem):
         from PIL import Image
-        for key,value in problem.figures.items():
-            img = Image.open(value.visualFilename)
-            img.show()
-        return -1
+        import numpy as np
+        img = {}  # clues
+        imgX = {}  # solutions
+        angel_set = np.array([0, 30, 45, 60, 90])  # remember to fill the background as white.
+        transpose_set = [Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM, Image.TRANSPOSE]
+
+        def test_transform(imgX, img, angel_set, transpose_set):  # imgX dic of
+            score1 = {}
+            score2 = {}
+            avg_score = {}
+            im_trans_1 = 10000  # initialize bigger value of for mse comparison
+            im_trans_2 = 10000
+            for key in imgX.items():
+                for angel in angel_set:
+                    for trans in transpose_set:
+                        im = Image.fromarray(img['A'])  # im is an image, img is an array
+
+                        im.transpose(trans).rotate(angel)
+                        tmp1 = np.add(np.substract(img['C'], np.array(im)), img['B'])  # C-A+B
+                        im_trans_1 = min(tmp1, im_trans_1)
+                        tmp2 = np.add(np.substract(img['B'], np.array(im)), img['C'])  # B-A+C
+                        im_trans_2 = min(tmp2, im_trans_2)
+                score1[key] = mse(imgX[key], im_trans_1)
+                score2[key] = mse(imgX[key], im_trans_2)
+            for key, value in score1:
+                avg_score[key] = float((score1[key] + score2[key]) / 2)
+            return max(avg_score, key=avg_score.get)
+
+        def mse(imageA, imageB):
+            # the 'Mean Squared Error' between the two images is the
+            # sum of the squared difference between the two images;
+            # NOTE: the two images must have the same dimension
+            err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+            err /= float(imageA.shape[0] * imageA.shape[1])
+
+            # return the MSE, the lower the error, the more "similar"
+            # the two images are
+            return err
+
+
+
+        for key, value in problem.figures.items():
+            if problem.problemSetName[-1] == 'B':
+                if key.isalpha == True:
+                    img[key] = np.array(Image.open(value.visualFilename))
+                elif key.isdigit ==True:
+                    imgX[key] = np.array(Image.open(value.visualFilename))
+
+                answer = test_transform(imgX, img, angel_set, transpose_set)
+
+        return answer
--- Project-Code-Python/Problems/Basic Problems B/Basic Problem B-01/basicFun_v2.py
+++ Project-Code-Python/Problems/Basic Problems B/Basic Problem B-01/basicFun_v2.py
@@ -3,30 +3,33 @@ import os
 import numpy as np
 
 # In this file, I will implement rotate, fill, transponse, add/minus/identical, and compare similarity
-# os.chdir('Project-Code-Python/Problems/Basic Problems B/Basic Problem B-01/')
+# os.getcwd()
+os.chdir('Project-Code-Python/Problems/Basic Problems B/Basic Problem B-01/')
 imgA = np.array(Image.open('A.png'))
 imgB = np.array(Image.open('B.png'))
 imgC = np.array(Image.open('C.png'))
 
 
 
-angels = np.array([0,30,45,60,90])  #remember to fill the background as white.
-transpose_cat = [Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM, Image.TRANSPOSE]
+angel_set = np.array([0,30,45,60,90])  #remember to fill the background as white.
+transpose_set = [Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM, Image.TRANSPOSE]
+
+def test_transform():
 
 
 #detect line pixel changes from white to black and return the times of changes.
 # Just realize, fill is another way to add, no need to do that!
-def line_break_detect(line,white):
-    return p_start,p_end,breaks,white_or_black
+# def line_break_detect(line,white):
+#     return p_start,p_end,breaks,white_or_black
 
-def fill_halo(img):
-    line_breaks = np.zeros(img.shape[0],3)
-    if np.sum(img[0])/img.size == 255
-        white = 1
-    for line in img:
-        s,e,b,wb = line_break_detect(line,white)
-        line_breaks.append([s,e,b,wb])
-    patterns = None                         # find a way to identify white-black changes.
+# def fill_halo(img):
+#     line_breaks = np.zeros(img.shape[0],3)
+#     if np.sum(img[0])/img.size == 255
+#         white = 1
+#     for line in img:
+#         s,e,b,wb = line_break_detect(line,white)
+#         line_breaks.append([s,e,b,wb])
+#     patterns = None                         # find a way to identify white-black changes.
 
 # MSE for comparing differences between images.
 def mse(imageA, imageB):
