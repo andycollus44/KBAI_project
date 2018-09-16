739153843d6d94cc752a4fb530b5be2e0a14fc27
Qian Sun <s223554@gmail.com>
2018-09-16 16:08:14 -0400

    v02 finished with new pattern checking, involve transformation of both A,B,c

--- Project-Code-Python/Problems/tester_v2.py
+++ Project-Code-Python/Problems/tester_v2.py
@@ -1,4 +1,4 @@
-# tester for comparing C-A+B, B-A+C:
+# tester for T(B)-A and T(C)-X, get the one with minimum difference.
 
 from PIL import Image
 import numpy as np
@@ -6,12 +6,13 @@ import os,glob
 import time
 
 home = 'C:/Users/s2235/PycharmProjects/KBAI_project/Project-Code-Python/'
-problemset = 'Basic Problems B/Basic Problem B-04/'
+problemset = 'Basic Problems B/Basic Problem B-05/'
 os.chdir(home+'Problems/'+problemset)
 
 images = {}
 img = {}
 imgX= {}
+
 
 for filename in glob.glob('*.png'):
     if len(filename)==5:
@@ -25,6 +26,7 @@ for key, value in images.items():
     else:
         imgX[key] = np.array(Image.open(images[key]))
 
+
 angel_set = np.array([0])
 transpose_set = [-1,Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM, Image.TRANSPOSE,Image.ROTATE_90, Image.ROTATE_180,Image.ROTATE_270]
 
@@ -32,29 +34,35 @@ def test_transform(imgX, img, angel_set, transpose_set):  # imgX dic of
     score1 = {}
     score2 = {}
     avg_score = {}
-    im_trans_1 = 100000  # initialize bigger value of for mse comparison
-    im_trans_2 = 100000
+
     for key,value in imgX.items():
+        t_list1 = np.multiply(np.ones(100),500000)  # t_list to store MSE and T to get all valid transpose from B-A
+        t_list2 = np.multiply(np.ones(100),500000)
         for angel in angel_set:
-            for trans in transpose_set:
+            for i,trans in enumerate(transpose_set):
                 im = Image.fromarray(img['A'])  # im is an image, img is an array
-                try:
+                try:            # allow no transpose
                     im = im.transpose(trans).rotate(angel)
                 except ValueError:
                     im = im.rotate(angel)
-                tmp1 = np.add(np.subtract(img['C'], np.array(im)), img['B'])  # C-A+B
-                im_trans_1 = np.minimum(mse(tmp1,imgX[key]), im_trans_1)            #       smallest mse
-                tmp2 = np.add(np.subtract(img['B'], np.array(im)), img['C'])  # B-A+C
-                im_trans_2 = np.minimum(mse(tmp2,imgX[key]), im_trans_2)
+                tmpTAB = np.subtract(np.array(im),img['B'])  # T(A)-B
+                tmpTCX = np.subtract(img['C'],imgX[key])         # T(C)-X
+                t1 = mse(tmpTAB,tmpTCX)           #       the transposed image difference
+                t_list1[i]=t1                       #dic for all mse, to get minimum
+                tmpTAC = np.subtract(np.array(im), img['C'])  # T(A)-C
+                tmpTBX = np.subtract(img['B'], imgX[key])  # T(C)-X
+                t2 = mse(tmpTAC,tmpTBX)
+                t_list2[i]=t2
                 # check points
-                print(key+' C-A+B '+str(im_trans_1))
-                Image.fromarray(tmp1).show()
-                pause()
-                print(key + ' B-A+C '+str(im_trans_2))
-                Image.fromarray(tmp2).show()
-                pause()
-        score1[key] = im_trans_1
-        score2[key] = im_trans_2
+                # print(key+' C-A+B '+str(im_trans_1))
+                # Image.fromarray(tmp1).show()
+                # pause()
+                # print(key + ' B-A+C '+str(im_trans_2))
+                # Image.fromarray(tmp2).show()
+                # pause()
+        score1[key] = min(t_list1)
+        score2[key] = min(t_list2)
+
     ans = 0
     for key, value in score1.items():
         avg_score[key] = float((score1[key] + score2[key]) / 2)
@@ -67,6 +75,7 @@ def test_transform(imgX, img, angel_set, transpose_set):  # imgX dic of
         print(key+' '+str(value))
     return ans
 
+
 def mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
     # sum of the squared difference between the two images;
@@ -78,8 +87,5 @@ def mse(imageA, imageB):
     # the two images are
     return err
 
-#pause for debug
-def pause():
-    programPause = input("Press the <ENTER> key to continue...")
-
 answer = test_transform(imgX, img, angel_set, transpose_set)
+print(answer)
