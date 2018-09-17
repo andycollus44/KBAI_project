7d2ee304ffe7c4d36dfe04f835049043887e245e
Qian Sun <s223554@gmail.com>
2018-09-16 20:08:13 -0400

    go on debugging.

--- Project-Code-Python/Problems/tester_v2.py
+++ Project-Code-Python/Problems/tester_v2.py
@@ -2,57 +2,65 @@
 
 from PIL import Image
 import numpy as np
-import os,glob
+import os, glob
 import time
 
 home = 'C:/Users/s2235/PycharmProjects/KBAI_project/Project-Code-Python/'
 problemset = 'Basic Problems B/Basic Problem B-05/'
-os.chdir(home+'Problems/'+problemset)
+os.chdir(home + 'Problems/' + problemset)
 
 images = {}
 img = {}
-imgX= {}
+imgX = {}
 
+def img_threshold(img, thres):      # better for subtraction
+    img_tmp = np.array(img)
+    img_tmp[img > thres] = 1  # white to 1
+    img_tmp[img <= thres] = 2  # black to 2
+    return img_tmp
 
 for filename in glob.glob('*.png'):
-    if len(filename)==5:
-        images[filename[0]]=home+'Problems/'+problemset+filename
+    if len(filename) == 5:
+        images[filename[0]] = home + 'Problems/' + problemset + filename
 
 for key, value in images.items():
 
     if key.isalpha() == True:
-        img[key] = np.array(Image.open(images[key]))
+        image_array = np.array(Image.open(images[key]))
+        img[key] = img_threshold(image_array, 128)
     # else key.isdigit() ==True:
     else:
-        imgX[key] = np.array(Image.open(images[key]))
-
+        imgX[key] = img_threshold(np.array(Image.open(images[key])), 128)
 
 angel_set = np.array([0])
-transpose_set = [-1,Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM, Image.TRANSPOSE,Image.ROTATE_90, Image.ROTATE_180,Image.ROTATE_270]
+transpose_set = [-1, Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM, Image.TRANSPOSE, Image.ROTATE_90, Image.ROTATE_180,
+                 Image.ROTATE_270]
+
 
 def test_transform(imgX, img, angel_set, transpose_set):  # imgX dic of
     score1 = {}
     score2 = {}
     avg_score = {}
 
-    for key,value in imgX.items():
-        t_list1 = np.multiply(np.ones(100),500000)  # t_list to store MSE and T to get all valid transpose from B-A
-        t_list2 = np.multiply(np.ones(100),500000)
+    for key, value in imgX.items():
+        t_list1 = []  # t_list to store MSE and T to get all valid transpose from B-A
+        t_list2 = []
         for angel in angel_set:
-            for i,trans in enumerate(transpose_set):
+            for trans in transpose_set:
                 im = Image.fromarray(img['A'])  # im is an image, img is an array
-                try:            # allow no transpose
+                try:  # allow no transpose
                     im = im.transpose(trans).rotate(angel)
                 except ValueError:
                     im = im.rotate(angel)
-                tmpTAB = np.subtract(np.array(im),img['B'])  # T(A)-B
-                tmpTCX = np.subtract(img['C'],imgX[key])         # T(C)-X
-                t1 = mse(tmpTAB,tmpTCX)           #       the transposed image difference
-                t_list1[i]=t1                       #dic for all mse, to get minimum
+
+                tmpTAB = np.subtract(np.array(im), img['B'])  # T(A)-B
+                tmpTCX = np.subtract(img['C'], imgX[key])  # T(C)-X
+                t1 = mse(tmpTAB, tmpTCX)  # the transposed image difference
+                t_list1.append(t1)  # dic for all mse of differences, to get minimum
                 tmpTAC = np.subtract(np.array(im), img['C'])  # T(A)-C
                 tmpTBX = np.subtract(img['B'], imgX[key])  # T(C)-X
-                t2 = mse(tmpTAC,tmpTBX)
-                t_list2[i]=t2
+                t2 = mse(tmpTAC, tmpTBX)
+                t_list2.append(t2)
                 # check points
                 # print(key+' C-A+B '+str(im_trans_1))
                 # Image.fromarray(tmp1).show()
@@ -71,8 +79,8 @@ def test_transform(imgX, img, angel_set, transpose_set):  # imgX dic of
             ans = min(avg_score, key=avg_score.get)
         except ValueError:
             print(avg_score)
-    for key,value in avg_score.items():
-        print(key+' '+str(value))
+    for key, value in avg_score.items():
+        print(key + ' ' + str(value))
     return ans
 
 
@@ -86,6 +94,11 @@ def mse(imageA, imageB):
     # return the MSE, the lower the error, the more "similar"
     # the two images are
     return err
+
+def transform_least_mse(imgA,imgB,trans,angel):
+    return -1
+
+
 
 answer = test_transform(imgX, img, angel_set, transpose_set)
 print(answer)
--- Project-Code-Python/submit.py
+++ Project-Code-Python/submit.py
@@ -21,7 +21,7 @@ def main():
   app_data_dir = os.path.abspath(".bonnie")
 
   files = args.files
-  agent_file = 'Agent_v01.py'
+  agent_file = 'Agent.py'
   language_file = 'language.txt'
   if agent_file not in files:
     files.append(agent_file)
